b14ce11c322c4c0f7e21e826e4ca4195f3e5863a
Joshua Skelton <joshua.skelton@gmail.com>
2014-08-26 22:37:15 -0700

    Initial Commit

---
+++ pal2cube/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pal2cube
+//
+//  Created by Joshua on 8/26/14.
+//  Copyright (c) 2014 Joshua Skelton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
